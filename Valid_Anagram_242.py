46e3c55b5c3f4f0aba7ec3d88428842163b69e83
Loyd Ngei <loydngei@gmail.com>
2023-11-21 16:50:20 +0300

    Valid Anagram

--- Valid_Anagram_242.py
+++ Valid_Anagram_242.py
@@ -21,7 +21,7 @@ class solution:
         # Count Character Occurrences in s and t
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
 
         # Compare Counts
         for c in countS:
