41549b35449edac2d6b2f2c93874747d2658ad2a
Loyd Ngei <loydngei@gmail.com>
2024-01-26 17:04:33 +0300

    pattern 2

---
+++ StriversA2Z/2_pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def printTriangle(self, N:int):
+        # Code here
+        
+        
+        for i in range(1, N+1):
+            for j in range(i):
+                print("* ", end="")
+            print()
+
+
+if __name__ == "__main__":
+    t = int(input("Enter the number of test cases: "))
+    for _ in range(t):
+        N = int(input("Enter the input: "))
+        ob = Solution()
+        ob.printTriangle(N)
