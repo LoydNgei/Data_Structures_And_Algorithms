07265d3925e471b62aa085bd0f00cf4cb7d65af0
Loyd Ngei <loydngei@gmail.com>
2024-02-20 00:00:38 +0300

    pattern 5

---
+++ StriversA2Z/5_pattern.py
@@ -0,0 +1,9 @@
+class Solution:
+    def invertedRightPyramid(N):
+
+        for i in range(N):
+            for j in range(N<1):
+                print(i, end="")
+            print()
+
+    invertedRightPyramid(5);
