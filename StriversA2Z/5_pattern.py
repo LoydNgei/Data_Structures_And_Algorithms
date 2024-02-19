13a2062239fce65fcb06e7bb83d4903c5df4c3da
Loyd Ngei <loydngei@gmail.com>
2024-02-20 00:11:53 +0300

    pattern 5 inverting right pyramid

--- StriversA2Z/5_pattern.py
+++ StriversA2Z/5_pattern.py
@@ -1,9 +1,9 @@
 class Solution:
     def invertedRightPyramid(N):
 
-        for i in range(N):
-            for j in range(N<1):
-                print(i, end="")
+        for i in range(N, 0, -1):
+            for j in range(i):
+                print("*", end="")
             print()
 
-    invertedRightPyramid(5);
+    invertedRightPyramid(3);
