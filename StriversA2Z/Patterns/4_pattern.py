08bcc78e31402572cbb962d51d6d020ea5229b79
Loyd Ngei <loydngei@gmail.com>
2024-11-10 10:29:11 +0300

    DSA

---
+++ StriversA2Z/Patterns/4_pattern.py
@@ -0,0 +1,8 @@
+class Solution:
+    def printTriangle(N):
+        # Code here
+        for i in range(N+1):
+            for j in range(i):
+                print(i, end=" ")
+            print()
+    printTriangle(5)
---
+++ StriversA2Z/Patterns/5_pattern.py
@@ -0,0 +1,7 @@
+class Solution:
+    def invertedRightPyramid(N):
+        for i in range(N, 0, -1):
+            for j in range(i):
+                print("*", end="")
+            print()
+    invertedRightPyramid(8);
