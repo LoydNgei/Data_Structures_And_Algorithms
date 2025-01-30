1e97924c2adb81a00b0a8d4617d2ac8ed0bd1008
Loyd Ngei <loydngei@gmail.com>
2025-01-30 18:39:57 +0300

    Two sum problem

---
+++ Leetcode/two_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        h = {}
+        for i in range(len(nums)):
+            i = h[i]
+
+        for i in range(len(nums)):
+            
